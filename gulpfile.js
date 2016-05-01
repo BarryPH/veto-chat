1922ea96bb9f61971650d63c159b43121aef984f
Barry Hood <BarryPH@protonmail.com>
2016-05-01 19:13:40 +1200

    Linting and testing added to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
+'use strict';
+
 var gulp = require('gulp');
-var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
+var mocha = require('gulp-spawn-mocha');
+var jshint = require('gulp-jshint');
+var jshintReporter = require('jshint-stylish');
 
 gulp.task('browserSync', function() {
 	browserSync({
 		open: false,
-		proxy: 'localhost:3000'
+		notify: false,
+		proxy: 'localhost:3000',
+		port: 3001,
 	});
 });
 
@@ -18,8 +25,20 @@ gulp.task('sass', function() {
 		}));
 });
 
-gulp.task('watch', ['browserSync', 'sass'], function() {
+gulp.task('test', function() {
+	return gulp.src('tests/**/*.js')
+		.pipe(mocha());
+});
+
+gulp.task('lint', ['test'], function() {
+	return gulp.src(['**/*.js', '!node_modules/**/*.js'])
+		.pipe(jshint())
+		.pipe(jshint.reporter(jshintReporter));
+});
+
+gulp.task('watch', ['browserSync', 'sass', 'lint'], function() {
 	gulp.watch('public/scss/**/*.scss', ['sass']);
 	gulp.watch('views/**/*.ejs', browserSync.reload);
 	gulp.watch('public/js/**/*.js', browserSync.reload);
+	gulp.watch(['**/*.js', '!node_modules/**/*.js'], ['lint']);
 });
---
+++ tests/socketio.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var expect = require('chai').expect;
+var mocha = require('mocha');
+var io = require('socket.io-client');
